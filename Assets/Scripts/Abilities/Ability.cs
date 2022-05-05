269e6a02a255b33b03f5e79d16971c7eea8434fc
Stefano Fiumara <stefano.fiumara@slalom.com>
2022-05-05 17:38:59 -0500

    refactor ability base class to manage all cooldowns

--- Assets/Scripts/Abilities/Ability.cs
+++ Assets/Scripts/Abilities/Ability.cs
@@ -1,22 +1,84 @@
+using Events;
 using UnityEngine;
 
 namespace Abilities
 {
-    public class Ability : MonoBehaviour
+    public abstract class Ability : MonoBehaviour
     {
-        protected bool _isAbilityActive  = false;
-        protected bool _isInternalCoolDownActive = false;
+        // How long in takes for Stamina to replenish
+        [SerializeField] private int RefreshCooldown;
 
-        // Amount of time between Ability usages
-        [SerializeField] protected int COOLDOWN_IN_SECONDS;
-        
         // Minimum amount of time between Ability usages
-        [SerializeField] protected int INTERNAL_COOLDOWN_IN_SECONDS;
-        
-        // Time length of Ability
-        [SerializeField] protected float DURATION_IN_SECONDS;
+        [SerializeField] private int UsageCooldown;
 
-        
-        
+        // Time length of Ability
+        [SerializeField] private float AbilityDuration;
+
+        // Internal variables to track above timers
+        private bool _isAbilityActive;
+        private bool _isUsageCooldownActive;
+
+        private float _currentRefreshCooldown;
+        private float _currentUsageCooldown;
+        private float _currentDuration;
+
+        public bool IsAbilityActive() => _isAbilityActive;
+        public bool IsTriggerable() => !_isAbilityActive && !_isUsageCooldownActive;
+
+        public void Start()
+        {
+            _isAbilityActive = false;
+            _isUsageCooldownActive = false;
+            _currentRefreshCooldown = 0f;
+            _currentDuration = 0f;
+            _currentUsageCooldown = 0f;
+        }
+
+        public void Update()
+        {
+            // Ability Duration Timer
+            if (_isAbilityActive)
+            {
+                _currentDuration += Time.deltaTime;
+                if (_currentDuration >= AbilityDuration)
+                {
+                    _isAbilityActive = false;
+                    _currentDuration = 0f;
+                }
+            }
+
+            // Usage Cooldown Timer
+            if (_isUsageCooldownActive)
+            {
+                _currentUsageCooldown += Time.deltaTime;
+                if (_currentUsageCooldown >= UsageCooldown)
+                {
+                    _currentUsageCooldown = 0f;
+                    _isUsageCooldownActive = false;
+                }
+            }
+
+            // Refresh Cooldown Timer
+            _currentRefreshCooldown += Time.deltaTime;
+            if (_currentRefreshCooldown >= RefreshCooldown)
+            {
+                Actions.RefreshDashComplete?.Invoke();
+                _currentRefreshCooldown = 0f;
+            }
+        }
+
+        public void Trigger()
+        {
+            if (IsTriggerable())
+            {
+                _isAbilityActive = true;
+                _isUsageCooldownActive = true;
+
+                _currentRefreshCooldown = 0f;
+                TriggerAbility();
+            }
+        }
+
+        protected abstract void TriggerAbility();
     }
 }
--- Assets/Scripts/Abilities/Dash.cs
+++ Assets/Scripts/Abilities/Dash.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections;
 using Events;
 using UnityEngine;
 
@@ -7,84 +5,11 @@ namespace Abilities
 {
     public class Dash : Ability
     {
-        // Minimum amount of time between Dashes  
-        private bool _isResetDashRefreshTimer;
-
         [SerializeField] public float DashVelocity;
-        public void Start()
+
+        protected override void TriggerAbility()
         {
-            StartCoroutine(RefreshDash());
-        }
-
-        public bool IsTriggerable()
-        {
-            return !_isAbilityActive && !_isInternalCoolDownActive;
-        }
-
-        public void Trigger()
-        {
-            if (IsTriggerable()) {
-                Actions.DashExecuted?.Invoke();
-
-                _isAbilityActive = true;
-                StartCoroutine(DashDuration());
-
-                // restore stamina point after X seconds
-                _isResetDashRefreshTimer = true;
-
-                // don't allow for use of Dash TOO fast
-                _isInternalCoolDownActive = true;
-                StartCoroutine(ProcessInternalDashCooldown());
-            }
-        }
-
-        private IEnumerator DashDuration()
-        {
-            Debug.Log("DashDuration");
-            float totalTime = 0f;
-            while (totalTime <= DURATION_IN_SECONDS) {
-                totalTime += Time.deltaTime;
-                yield return null;
-            }
-
-            _isAbilityActive = false;
-        }
-
-        private IEnumerator RefreshDash()
-        {
-            Debug.Log("RefreshDash");
-            float totalTime = 0f;
-
-            while (totalTime <= COOLDOWN_IN_SECONDS) {
-                if (_isResetDashRefreshTimer) {
-                    totalTime = 0f;
-                    _isResetDashRefreshTimer = false;
-                }
-
-                totalTime += Time.deltaTime;
-                yield return null;
-            }
-
-            Actions.RefreshDashComplete?.Invoke();
-            
-            StartCoroutine(RefreshDash());
-        }
-
-        private IEnumerator ProcessInternalDashCooldown()
-        {
-            float totalTime = 0f;
-
-            while (totalTime <= INTERNAL_COOLDOWN_IN_SECONDS) {
-                totalTime += Time.deltaTime;
-                yield return null;
-            }
-
-            _isInternalCoolDownActive = false;
-        }
-
-        public bool IsAbilityActive()
-        {
-            return _isAbilityActive;
+            Actions.DashExecuted?.Invoke();
         }
     }
 }
