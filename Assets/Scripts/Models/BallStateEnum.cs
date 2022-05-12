43bcbe73ff27b1732e800ef427b568c05cbab60d
Ryan Henderson <htxryan@gmail.com>
2022-05-11 20:40:35 -0500

    Ability for player to pick up and throw ball added (#4)
    * Adding player ability to pick up and throw ball
    
    * Addressing PR comments

---
+++ Assets/Scripts/Models/BallStateEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace Models
+{
+    public enum BallStateEnum
+    {
+        Unknown = 0,
+        Stationary = 1,
+        Moving = 2
+    }
+}
--- Assets/Scripts/Player/BallMovement.cs
+++ Assets/Scripts/Player/BallMovement.cs
@@ -1,3 +1,4 @@
+using Models;
 using UnityEngine;
 
 namespace Player
@@ -5,12 +6,30 @@ namespace Player
     public class BallMovement : MonoBehaviour
     {
         public float Velocity;
+        public float Friction;
+        public float FrictionThreshold;
         public Vector3 Direction;
+        public BallStateEnum BallState;
+        public TeamAffiliationEnum TeamAffiliation;
+
+        private void Start()
+        {
+            BallState = BallStateEnum.Stationary;
+        }
 
         // Update is called once per frame
         public void Update()
         {
-            transform.position += (Velocity * Time.deltaTime * Direction.normalized);
+            if (BallState == BallStateEnum.Moving)
+            {
+                transform.position += (Velocity * Time.deltaTime * Direction.normalized);
+                Velocity -= (Friction * Time.deltaTime);
+                if (Velocity < FrictionThreshold)
+                {
+                    BallState = BallStateEnum.Stationary;
+                    Velocity = 0;
+                }
+            }
         }
     }
 }
